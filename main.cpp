d75cf0cb42b29ecd974e20f150076d33a67c6b83
molecule1100 <damnfker@gmail.com>
2022-12-23 17:23:24 +0200

    fixed main.cpp

--- main.cpp
+++ main.cpp
@@ -4,23 +4,11 @@
 
 using namespace sf;
 
-struct Car
+Car::Car()
 {
-    float x, y, speed, angle;
-
-    Car()
-    {
-        x = 0; y = 0;
-        speed = 2; angle = 0;
-    }
-
-    void move()
-    {
-        x += sin(angle) * speed;
-        y -= cos(angle) * speed;
-    }
-};
-
+    x = 0; y = 0;
+    speed = 2; angle = 0;
+}
 
 int main()
 {
